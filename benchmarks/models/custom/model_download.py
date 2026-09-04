caf63bad747007c3fa7c635e549d3100bb4a9be2
Franciszek Zachuta <franciszek.zachuta@gmail.com>
2026-09-04 11:03:06 +0200

    Changed default model

--- benchmarks/models/custom/model_download.py
+++ benchmarks/models/custom/model_download.py
@@ -3,7 +3,7 @@ import gdown
 import os
 
 def download_model():
-    file_id = "1xlMXgoJdesRE-ZGWBdMTXVo4GyD0JEFC"
+    file_id = "177lLcpWqapeBtIe2d33uXehCTQ8Vj1Rd"
     url = f"https://drive.google.com/uc?id={file_id}"
     path = str(Path(os.path.dirname(os.path.abspath(__file__))) / "detector.pth")
 
